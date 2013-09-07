118e2d204f524ea946e8ec7100d2a6bed83b42c8
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2013-09-07 07:12:37 -0400

    More progress... getting breakfast

--- redirectTab.js
+++ redirectTab.js
@@ -1,4 +1,19 @@
 var key = "refocus_bookmarks";//storage key
+//just copied and pasted, I'm sure we can split this up
+function parseStr(linkStr){
+	var linkArr = [];
+	if(!linkStr){
+		return linkArr;
+	}
+	var allBookmarks = linkStr.split("\t");
+	console.log(allBookmarks);
+	for(var i = 0; i < allBookmarks.length; i++){
+		var parts = allBookmarks[i].split(" ");
+		linkArr.push({url: parts[0], title: parts[1]});
+	}
+	console.log(linkArr);
+	return linkArr;
+}
 
 //when user opens new tab, based on recent history, chance of opening a page on their 'to-read' list
 chrome.tabs.onCreated.addListener(function(tab) {
@@ -6,14 +21,13 @@ chrome.tabs.onCreated.addListener(function(tab) {
 
 	//get random link from todo list
 	chrome.storage.sync.get(key, function(data){
-		var linkStr = data[key];
-		if(!linkStr || linkStr.length == 0){
+		var linkArr = parseStr(data[key]);
+		if(linkArr.length == 0){
 			return;//no links to use
 		}
-		var allBookmarks = linkStr.split(" ");
-		var rand = Math.floor(Math.random() * allBookmarks.length);
+		var rand = Math.floor(Math.random() * linkArr.length);
 		if(Math.floor(Math.random() * 2) == 0){//just for testing, 1/2 chance of showing
-			chrome.tabs.update(tab.id, {url: allBookmarks[rand]});
+			chrome.tabs.update(tab.id, {url: linkArr[rand].url});
 		}
 	});
 
--- saveBookmark.js
+++ saveBookmark.js
@@ -1,52 +1,89 @@
 var key = "refocus_bookmarks";
+console = chrome.extension.getBackgroundPage().console;
 
-function linkExists(linkStr, link){
-	var allBookmarks = linkStr.split(" ");
+/*
+links are stored the following:
+<url> <title>\t<url> <title>\t...
+*/
+/*
+TODO: implement the parseStr method, store titles of webpages
+*/
+//chrome.storage.sync.clear();
+function parseStr(linkStr){
+	var linkArr = [];
+	if(!linkStr){
+		return linkArr;
+	}
+	var allBookmarks = linkStr.split("\t");
+	console.log(allBookmarks);
 	for(var i = 0; i < allBookmarks.length; i++){
-		if(allBookmarks[i] == link){
+		var parts = allBookmarks[i].split(" ");
+		linkArr.push({url: parts[0], title: parts[1]});
+	}
+	console.log(linkArr);
+	return linkArr;
+}
+
+function toStr(linkArr){
+	var str = "";
+	for(var i = 0; i < linkArr.length; i++){
+		if(i > 0){
+			str += "\t";
+		}
+		str += linkArr[i].url + " " + linkArr[i].title;
+	}
+	return str;
+}
+
+function linkExists(linkArr, link){
+	for(var i = 0; i < linkArr.length; i++){
+		if(linkArr[i].url == link.url){
 			return true;
 		}
 	}
 	return false;
 }
 
-function removeLink(linkStr, link){
-	var newStr = "";
-	var allBookmarks = linkStr.split(" ");
-	for(var i = 0; i < allBookmarks.length; i++){
-		if(allBookmarks[i] == link){
-			continue;
+function removeLink(linkArr, link){
+	for(var i = 0; i < linkArr.length; i++){
+		if(linkArr[i].url == link.url){
+			linkArr.splice(i, 1);
 		}
-		if(newStr != ""){
-			newStr += " ";
-		}
-		newStr += allBookmarks[i];
 	}
-	return newStr;
 }
 
 function storeLinkStr(linkStr){
+	console.log("STORING: " + linkStr);
 	var newObj = {};
 	newObj[key] = linkStr;
 	chrome.storage.sync.set(newObj, function(){});
 }
 
-$(document).ready(function(){
-	//test
+function updateList(linkArr){
+	$("ul").empty();
+	//console.log(linkStr + " : " );
+	for(var i = 0; i < linkArr.length; i++){
+		$("ul").append("<li>" + linkArr[i].title + "<a href='#' class='remove'>X</a></li>");
+	}
+}
 
-	var tabUrl = "";
-	var linkStr = "";
+$(document).ready(function(){
+	var curTab = {};
+	var linkArr = [];
 	var stat = $("#status");
 	chrome.tabs.getSelected(function(tab){
-		
 		//check if already in storage
-		tabUrl = tab.url;
+		curTab = {
+			url: tab.url,
+			title: tab.title
+		};
+
 		chrome.storage.sync.get(key, function(data){
-			linkStr = data[key];
-			if(!linkStr){
-				linkStr = "";
-			}
-			if(linkExists(linkStr, tabUrl)){
+			console.log("1");
+			console.log(data[key]);
+			linkArr = parseStr(data[key]);
+			console.log("2");
+			if(linkExists(linkArr, curTab)){
 				$("#add").hide();
 				$("#remove").show();
 			}
@@ -54,6 +91,7 @@ $(document).ready(function(){
 				$("#add").show();
 				$("#remove").hide();
 			}
+			updateList(linkArr);
 		});
 	});
 
@@ -61,38 +99,32 @@ $(document).ready(function(){
 		$(this).hide();
 		e.preventDefault();
 		//check if bookmark exists
-		console = chrome.extension.getBackgroundPage().console;
-		var empty = false;
-		var newValue = "";
-
-		if(!linkStr || linkStr == ""){
-			//linkStr empty
-			empty = true;
-			linkStr = "";
-		}
-
 		//add link
-		if(!linkExists(linkStr, tabUrl)){
-			if(linkStr != ""){
-				linkStr += " ";
-			}
-			linkStr += tabUrl;
+		if(!linkExists(linkArr, curTab)){
+			linkArr.push(curTab);
 			stat.html("Page bookmarked");
 		}
 		else{
 			//already there
 			stat.html("Bookmark already exists");
 		}
-		storeLinkStr(linkStr);
+		storeLinkStr(toStr(linkArr));
 		$("#remove").show();
-	});		
+		updateList(linkArr);
+	});
 	$("#remove").click(function(e){
 		$(this).hide();
 		$("#add").show();
 		e.preventDefault();
-		linkStr = removeLink(linkStr, tabUrl);
+		removeLink(linkArr, curTab);
 		stat.html("Bookmark removed");
-		storeLinkStr(linkStr);
+		storeLinkStr(toStr(linkArr));
+		updateList(linkArr);
 	}).show();
+
+	$(".remove").on(function(e){
+		console.log("Removing...");
+	})
+	
 });
 
